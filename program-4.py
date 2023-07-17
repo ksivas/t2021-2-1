6e4db83e7347781c7b741c9fa5f32390cb7e4cbb
ksivas <117575646+ksivas@users.noreply.github.com>
2023-07-17 22:22:21 +0530

    program-4.py

--- program-4.py
+++ program-4.py
@@ -1,13 +1,13 @@
-list=list(map(int,input().split()))
+lst=list(map(int, input().split(',')))
 a=[1,2,3,4,5,6,7,8,9]
-dic={}
+dit={}
 
 for i in a:
    count=0;
-   for j in list:
+   for j in lst:
       if(j%i==0):
          count+=1
-   dic[i]=count
+   dit[i]=count
         
-print(dic)
+print(dit)
    
